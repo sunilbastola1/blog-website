1d5b247c402fdce079c0ab07ab02ace18555fe8b
sunilbastola <41153362+sunilbastola@users.noreply.github.com>
2023-09-07 13:29:18 +0545

    Added the skeleton data for MYSQL created config folder, moderl folder, added to mysql

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // Requiring express in the project
-const express = require("express")
+const express = require("express");
+const { blogs } = require("./model/index");
 const app = express()
+
+
+// Import database
+require("./model/index")
+ 
 
 // Setting the view engine for ejs to find the .ejs file by app.js
 app.set("view engine", "ejs");
@@ -20,8 +26,25 @@ app.get("/createblog", (req,res) => {
     res.render("createBlog")
 });
 
-app.post("/createblog",(req,res) =>{
-    console.log(req.body)
+app.post("/createblog",async (req,res) =>{
+    // Selecting the data from form input in website
+    // First approach
+    const title = req.body.title
+    const subtitle = req.body.subtitle
+    const description = req.body.description
+    
+    //Second Approach
+    // const {title, subtitle, description} = req.body
+    console.log(title, subtitle, description)
+
+    // put data into database
+    res.send("Your blog has been successfully submitted.")
+    // For sending the input data into blogs database
+    await blogs.create({   // await needs async i.e. takes some time to put data into db
+        title : title,
+        subtitle : subtitle,
+        description : description
+    })
 })
 
 
